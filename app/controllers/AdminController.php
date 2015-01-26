852604c842343be5b772997109141d5d28058e72
Mithun <mithun@grameentek.com>
2015-01-27 00:18:46 +0600

    img

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -9,16 +9,16 @@ class AdminController extends BaseController {
     protected $layout = 'layout';
 
     public function admin() {
-        
+
         if (Request::isMethod('post')) {
             $product_name = Input::get('product_name');
             $product_price = Input::get('product_price');
-            $product_img = Input::file('product_img')->move('/var/www/laravel/product_img', Input::file('product_img')->getRealPath());
-            
+            $product_img = Input::file('product_img')->move(public_path() . '/product_img', Input::file('product_img')->getClientOriginalName());
+
             $product = new Product();
             $product->product_name = $product_name;
             $product->product_price = $product_price;
-            $product->product_img = $product_img;
+            $product->product_img = 'product_img/' . Input::file('product_img')->getClientOriginalName();
             $product->save();
             Session::flash('Success_msg', 'You have successfuly Inserted a product');
         }
